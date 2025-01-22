d977a98dd3f3a0cdce0645c589e367ba5012fd67
MateusChavito <mateus.chavito@hotmail.com>
2025-01-22 13:52:01 -0300

    Deploy ready

--- src/main/java/com/mateuschaves/dslist/controllers/GameController.java
+++ src/main/java/com/mateuschaves/dslist/controllers/GameController.java
@@ -24,7 +24,7 @@ public class GameController {
         GameDTO result = gameService.findById(id);
         return result;
     }
-    
+
 
         @GetMapping
         public List<GameMinDTO> findAll () {
